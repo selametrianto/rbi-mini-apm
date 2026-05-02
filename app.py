0bcc342b1be68fe06f2c4e8833c606922856542b
selametrianto <selametrianto500@gmail.com>
2026-05-02 22:22:50 +0700

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(page_title="RBI Mini APM", layout="wide")
+
+st.title("RBI Mini APM System")
+st.write("Prototype running")
+
+st.metric("Total Assets", 413)
+st.metric("High Risk", 261)
